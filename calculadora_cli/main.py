ce2f83fb198c5e608dd2f27c775b1f90916ed11f
devid <nnick4673@gmail.com>
2023-03-11 10:21:18 -0300

    Soma com números reais

--- calculadora_cli/main.py
+++ calculadora_cli/main.py
@@ -17,6 +17,19 @@ def add(numeros: list[int]) -> None:
 	print(f'A soma vale: {soma}')
 
 @app.command()
+def addF(numeros: list[float]) -> None:
+	"""
+		Este comando recebe como parâmetros números reais 
+		(no formato x1 x2 x3 x4...) e retorna a soma deles.
+	"""
+	soma = 0
+
+	for n in numeros:
+		soma += n
+
+	print(f'A soma vale: {soma}')
+
+@app.command()
 def even(numeros: list[int]) -> None:
 	"""
 		Este comando recebe como parâmetros números inteiros
